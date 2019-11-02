5d8ece0ad1fecf20d0909e9ee850d7fccb87c149
Mary West <westmarye1219@gmail.com>
2019-11-01 20:51:07 -0500

    setup complete

---
+++ capstoneapi/urls.py
@@ -0,0 +1,28 @@
+"""capstoneapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ sizeyourdriveapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SizeyourdriveapiConfig(AppConfig):
+    name = 'sizeyourdriveapi'
